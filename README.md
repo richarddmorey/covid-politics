b7a5d9ed8ce65240845216a224ec39e0950e2ebd
Richard Morey <richarddmorey@gmail.com>
2021-07-09 19:24:11 +0100

    initial commit

--- README.md
+++ README.md
@@ -1,2 +1,24 @@
 # covid-politics
 Data analysis: Covid vaccination in US by Trump vote share
+
+This is an attempt to replicate and extend the graph in this tweet:
+https://twitter.com/charles_gaba/status/1413499252177780737
+
+Code is written in R and uses `renv` to manage dependencies.
+
+This is just a test of `gganimate` - I am less familiar with the intricacies of these data sets, so *caveat emptor*. This is just a toy analysis for me. Any hints where I went wrong interpreting the data/computing summaries would be appreciated. Issues include:
+
+* Differences between states in reporting in the election data (how/whether a total is calculated)
+* Some counties have 0 vaccination rates throughout
+* "Unknown" counties in both data sets
+
+If you don't want to muck about with github, just download the project from the green "Code" button above. Choose "Download ZIP".
+
+To run:
+
+1. Install the `renv` package.
+2. Open the project in RStudio.
+3. Run `renv::restore()` to install the dependencies.
+4. Run the code in `analysis.R`.
+
+You may need to install binaries for `ffmpeg` to generate the video file, if you don't already have it installed.
---
+++ analysis.R
@@ -0,0 +1,112 @@
+
+# Attempt to replicate and extend:
+# https://twitter.com/charles_gaba/status/1413499252177780737
+
+library(dplyr)
+library(magrittr)
+library(lubridate)
+library(ggplot2)
+library(gganimate)
+library(av)
+
+# From https://www.ers.usda.gov/data-products/county-level-data-sets/download-data/
+here::here("data/population/PopulationEstimates.xls") %>%
+  readxl::read_xls(skip = 2) %>%
+  filter(substr(FIPStxt, 4, 5) != "00") %>%
+  rename(FIPS = FIPStxt) %>%
+  select(FIPS, POP_ESTIMATE_2019) -> population
+
+# From https://dataverse.harvard.edu/dataset.xhtml?persistentId=doi:10.7910/DVN/VOQCHQ
+here::here("data/elections/countypres_2000-2020.tab") %>%
+  readr::read_delim(delim = "\t") %>%
+  filter(
+    !is.na(county_fips), 
+    year == 2020,
+    candidate == "DONALD J TRUMP",
+    ) %>%
+  group_by(county_fips) %>%
+  summarise(
+    candidatevotes = sum(candidatevotes),
+    totalvotes = first(totalvotes)
+  ) %>%
+  mutate(trump_share = candidatevotes / totalvotes,
+         FIPS = stringi::stri_pad_left(county_fips, width=5, pad = "0")) %>%
+  select(FIPS, trump_share, totalvotes) -> election_data
+
+# From https://www.ers.usda.gov/data-products/county-level-data-sets/download-data/
+here::here("data/covid/COVID-19_Vaccinations_in_the_United_States_County.csv") %>%
+  readr::read_csv() %>%
+  filter(FIPS != "UNK") %>%
+  left_join(election_data, by = "FIPS") %>%
+  left_join(population, by = "FIPS") %>%
+  mutate(
+    Date = mdy(Date)
+  ) -> vaccine_data
+
+# Show missing data due to unknown vote share
+vaccine_data %>%
+  filter(Date == ymd("2021-07-08")) %>%
+  mutate(
+    trump_share_missing = is.na(trump_share)
+  ) %>%
+  group_by(trump_share_missing) %>%
+  summarise(n = n()) %>%
+  mutate(p = n / sum(n))
+
+# Show missing data due to unknown county population
+vaccine_data %>%
+  filter(Date == ymd("2021-07-08")) %>%
+  mutate(
+    pop_missing = is.na(POP_ESTIMATE_2019)
+  ) %>%
+  group_by(pop_missing) %>%
+  summarise(n = n()) %>%
+  mutate(p = n / sum(n))
+
+# Show unexplained 0 vaccination counts as of latest date
+vaccine_data %>%
+  filter(
+    Date == ymd("2021-07-08"),
+    Series_Complete_Yes == 0
+  ) %>%
+  select(trump_share, totalvotes, POP_ESTIMATE_2019, 
+         Recip_County, Recip_State, FIPS,
+         Series_Complete_Pop_Pct,
+         Series_Complete_Yes)
+
+frames_per_day = 2
+vaccine_data %>% pull(Date) %>% range() %>% diff() -> ndays
+
+## Animation
+vaccine_data %>%
+  ggplot(aes(x = trump_share*100, y = Series_Complete_Pop_Pct, 
+             size = POP_ESTIMATE_2019,
+             color = trump_share)) +
+  scale_size_area(name = "Population (2019)") +
+  scale_colour_viridis_c("Trump vote share (2020)") +
+  scale_y_continuous(name = "Complete vaccination (%)",
+                     limits = c(0,100), expand = c(0,0)) +
+  scale_x_continuous(name = "Trump share of vote (%)",
+                     limits = c(0,100), expand = c(0,0)) +
+  geom_point() +
+  hrbrthemes::theme_ipsum_rc() -> static_plot
+
+static_plot +
+  transition_states(states = Date, transition_length = 4, state_length = 1) + 
+  ease_aes('cubic-in-out') +
+  #view_follow(fixed_x = TRUE) +
+  labs(title = 'Percentage vaccinated by 2020 Trump support, {closest_state}', 
+       subtitle = "US Counties",
+       caption = "Data Sources: CDC, Harvard Dataverse, USDA") -> anim_plot
+
+final_animation<-animate(anim_plot, 
+                         nframes = as.integer(frames_per_day * ndays),
+                         fps = 16,
+                         width = 950, 
+                         height = 750, 
+                         renderer = av_renderer())
+
+anim_save(here::here("animation.webm"),
+          animation=final_animation)
+
+
